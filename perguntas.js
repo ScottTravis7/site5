5ed59aa6afcfe14a5617d2c81dff0e6f8d74d2f3
Travis Scott <rafinha57009125@gmail.com>
2024-11-20 17:21:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Qual o melhor time?',
+    'Corinthians ou Palmeiras?',
+    'Palmeiras'
+)
+
+criaCartao(
+    'Qual o melhor esporte?',
+    'Futebol ou Basquete?',
+    'Futebol'
+)
+
+criaCartao(
+    'Qual o melhor cantor?',
+    'Matue ou Teto',
+    'Matue'
+)
+
+criaCartao(
+    'O que é melhor?',
+    'Lasanha ou Chocolate?',
+    'Chocolate'
+)
